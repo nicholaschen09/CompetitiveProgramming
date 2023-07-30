fa7079aac43e6910a38886999b265488c4133ab0
Nicholas Chen <nicholas.chen243@gmail.com>
2023-07-30 10:15:51 -0400

    Create 2 P0.java

---
+++ AAC/2 P0.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class KoalaMatchmaking {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int a = 0;
+        int n = scan.nextInt();
+
+        a = n - 1;
+        System.out.println(a);
+    }
+}
